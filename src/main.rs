f9200b1dbf698abd35eda947af3251a4b554a106
Ivan Towlson <github@hestia.cc>
2021-08-05 13:31:20 +1200

    THe mighty kea

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
   println!("Content-Type: text/html\n");
   println!("<h1>Choose your bird</h1>");
-  println!("<p><img src='assets/images/kea.jpg'>Kea (photo: <a href='https://www.flickr.com/photos/27071649@N06'>Peti Deuxmont</a>, <a href='https://creativecommons.org/licenses/by/2.0/'>CC BY 2.0</a>, resized)</p>");
+  println!("<p><img src='assets/images/kea.jpg'>The MIGHTY Kea (photo: <a href='https://www.flickr.com/photos/27071649@N06'>Peti Deuxmont</a>, <a href='https://creativecommons.org/licenses/by/2.0/'>CC BY 2.0</a>, resized)</p>");
 }
